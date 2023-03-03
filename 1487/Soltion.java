dd4c844a4fe181abf6b976167a54edd9f6fe07c4
lumino <luminovoez@gmail.com>
2023-03-03 20:50:34 +0800

    1487

---
+++ 1487/Soltion.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] getFolderNames(String[] names) {
+        String[] r = new String[names.length];
+        for(int i=0; i<r.length; i++){
+            String n = names[i];
+            int no = 0;
+            String nn = n;
+            for(int j=0; j<i; j++){
+                if (r[j].equals(nn)) {
+                    no++;
+                    nn = n+"("+String.valueOf(no)+")";
+                }
+            }
+            r[i] = nn;
+        }
+        return r;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String [] names = new String[]{
+            "onepiece","onepiece(1)","onepiece"
+        };
+        sol.getFolderNames(names);
+    }
+}
