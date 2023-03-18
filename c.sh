abbd529cd483c2d0961e368f7c0ff8a0d9cb73b6
lumino <luminovoez@gmail.com>
2023-03-18 20:40:38 +0800

    create project script.

---
+++ c.sh
@@ -0,0 +1,15 @@
+# echo $0 // ./*.sh
+# echo $1 // first parameter
+
+mkdir $1
+echo "import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+" > "$1/Solution.java"
+
+code $1
