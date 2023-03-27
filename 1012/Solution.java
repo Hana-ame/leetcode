682ca1ff316b16c5367a5ab1939706236ebd0839
lumino <luminovoez@gmail.com>
2023-03-27 13:57:13 +0800

    1012 not clear

--- 1012/Solution.java
+++ 1012/Solution.java
@@ -3,7 +3,11 @@ import java.util.*;
 class Solution {
     public int numDupDigitsAtMostN(int n) {
        // 只要有两个数字相同就+1 
-       // 小
+       //     a b c d e f
+       // (a-1)*9*8*7*6*5
+       //     9*8*7*6*5*4
+       //   b*9*8*7*6
+       //     
     }
     public static void main(String[] args) {
         Solution solution = new Solution();
