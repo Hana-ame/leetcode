ee58c76c4aab7284cfcdf5706993586481b11855
lumino <luminovoez@gmail.com>
2023-03-27 10:45:13 +0800

    1574 M pass

---
+++ 1012/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int numDupDigitsAtMostN(int n) {
+       // 只要有两个数字相同就+1 
+       // 小
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
